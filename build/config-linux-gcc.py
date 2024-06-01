f46cd1c3d5542a11ddb898eb3a9d55c76279a52b
wgr45097 <guoruiwen021010@gmail.com>
2024-06-01 10:02:50 +0800

    Update config-linux-gcc.py to use clang

--- build/config-linux-gcc.py
+++ build/config-linux-gcc.py
@@ -2,8 +2,8 @@ import os, sys
 
 BUILDDIR       = '#build/release'
 DISTDIR        = '#dist'
-CXX            = 'g++'
-CC             = 'gcc'
+CXX            = 'clang++-10'
+CC             = 'clang-10'
 CXXFLAGS       = ['-O3', '-Wall', '-g', '-pipe', '-march=nocona', '-msse2', '-ftree-vectorize', '-mfpmath=sse', '-funsafe-math-optimizations', '-fno-rounding-math', '-fno-signaling-nans', '-fno-math-errno', '-fomit-frame-pointer', '-DMTS_DEBUG', '-DSINGLE_PRECISION', '-DSPECTRUM_SAMPLES=3', '-DMTS_SSE', '-DMTS_HAS_COHERENT_RT', '-fopenmp', '-fvisibility=hidden', '-mtls-dialect=gnu2']
 LINKFLAGS      = []
 SHLINKFLAGS    = ['-rdynamic', '-shared', '-fPIC', '-lstdc++']
